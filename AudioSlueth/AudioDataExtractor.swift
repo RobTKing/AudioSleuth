679596cfe46d8e7c3b2e8bcbfe3f8f2434824d14
robTKing <robtylerking@gmail.com>
2015-07-03 13:12:41 -0500

    add: create audio data extractor struct

---
+++ AudioSlueth/AudioDataExtractor.swift
@@ -0,0 +1,18 @@
+//
+//  AudioDataExtractor.swift
+//  AudioSlueth
+//
+//  Created by Robert King on 7/3/15.
+//  Copyright © 2015 AppTreats. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+struct AudioDataExtractor {
+    var error: NSError?
+    
+    func getDataFromAudioFile(audioURL : NSURL) {
+        
+    }
+}
