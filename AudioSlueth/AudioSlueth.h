b660040997de84067ee4c295810c482dfd65588c
robTKing <robtylerking@gmail.com>
2015-07-03 12:55:20 -0500

    Initial Commit

---
+++ AudioSlueth/AudioSlueth.h
@@ -0,0 +1,19 @@
+//
+//  AudioSlueth.h
+//  AudioSlueth
+//
+//  Created by Robert King on 7/3/15.
+//  Copyright © 2015 AppTreats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioSlueth.
+FOUNDATION_EXPORT double AudioSluethVersionNumber;
+
+//! Project version string for AudioSlueth.
+FOUNDATION_EXPORT const unsigned char AudioSluethVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioSlueth/PublicHeader.h>
+
+
