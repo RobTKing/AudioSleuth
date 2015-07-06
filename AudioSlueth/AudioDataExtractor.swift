0e8ad8fb8c055e7a41896f3b8e2e491acfb77963
robTKing <robtylerking@gmail.com>
2015-07-06 14:17:11 -0500

    ref: changed pointer variables to use right syntax

--- AudioSlueth/AudioDataExtractor.swift
+++ AudioSlueth/AudioDataExtractor.swift
@@ -14,21 +14,19 @@ struct AudioDataExtractor {
     // Properties for Audio
     
     func getDataFromAudioFile(audioURL : NSURL) {
-        var audioFileRef : UnsafeMutablePointer<ExtAudioFileRef>?
-        var defaultASBDRef : UnsafePointer<AudioStreamBasicDescription>?
+        var audioFileRef : ExtAudioFileRef?
+        var defaultASBD = createASBD()
         let bufferFrames = 4096
         var data = [Float](count: bufferFrames, repeatedValue: 0.0)
-        let defaultASBD = createASBD()
         
-        
-        let audioFileOpenError : OSStatus = ExtAudioFileOpenURL(audioURL, audioFileRef!)
+        let audioFileOpenError : OSStatus = ExtAudioFileOpenURL(audioURL, &audioFileRef!)
         
         // An error occured during file open, log the error
         if audioFileOpenError != noErr {
             NSLog("Cound not open audio URL, error: %@", audioFileOpenError)
         }
         
-        let audioFileSetError = ExtAudioFileSetProperty(audioFileRef!.memory, kExtAudioFileProperty_ClientDataFormat, UInt32(sizeofValue(defaultASBD)), defaultASBDRef!)
+        let audioFileSetError = ExtAudioFileSetProperty(audioFileRef!, kExtAudioFileProperty_ClientDataFormat, UInt32(sizeofValue(defaultASBD)), &defaultASBD)
         
         if audioFileSetError != noErr {
             NSLog("Cound not set audio file, error: %@", audioFileSetError)
